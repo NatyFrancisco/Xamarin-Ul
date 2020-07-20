91d79536af329383074cc40c43032557b4edca32
Naty Francisco <62029311+NatyFrancisco@users.noreply.github.com>
2020-07-20 12:19:54 -0400

    Formulario de Recepcion de Datos
    Asignacion 06

--- AppForm/AppForm/AppForm/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
+++ AppForm/AppForm/AppForm/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
@@ -17,8 +17,16 @@ namespace AppForm {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Entry MainEntry;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Entry MainEntry;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
+            MainEntry = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "MainEntry");
+            MainEntry = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "MainEntry");
         }
     }
 }
